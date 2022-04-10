915ab0f12f5943529c3be5ad5db1dea64173956c
leandroltf <leandroltf10@gmail.com>
2022-04-10 08:56:50 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'betbox-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'betbox-front';
+}
